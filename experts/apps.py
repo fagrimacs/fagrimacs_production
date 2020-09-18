1bf514853d57f79581dcdf64097075e23a14d3c1
Innocent Zenda <zendainnocent@gmail.com>
2020-09-18 11:30:58 +0300

    remove experts app

--- experts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExpertsConfig(AppConfig):
-    name = 'experts'
--- experts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import ExpertProfile
-
-
-class ExpertProfileForm(forms.ModelForm):
-    class Meta:
-        model = ExpertProfile
-        fields = ['company', 'website', 'region', 'street', 'profile_pic', ]
